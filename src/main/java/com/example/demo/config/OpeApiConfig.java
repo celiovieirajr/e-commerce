82c6ca30e7af8e03be5af50ea1c520df898fa716
Celio <juniorcelioata@gmail.com>
2026-01-28 00:11:32 -0300

    "Feat/learning Open API - Swagger"

---
+++ src/main/java/com/example/demo/config/OpeApiConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+//import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpeApiConfig {
+
+    @Bean
+    OpenAPI customOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("REST API's RESTful from 0 with Java, Spring boot and Docker")
+                .version("v1")
+                .description("REST API's RESTful from 0 with Java, Spring boot and Docker")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("https://swagger")));
+}
+}
